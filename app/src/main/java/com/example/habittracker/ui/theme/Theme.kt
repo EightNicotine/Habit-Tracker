6d77d28326120cd8142897987bf2b56f615e4bee
WellB2 <well.santanasft@gmail.com>
2026-02-24 00:25:03 -0300

    rollback Theme.kt

--- app/src/main/java/com/example/habittracker/ui/theme/Theme.kt
+++ app/src/main/java/com/example/habittracker/ui/theme/Theme.kt
@@ -23,7 +23,7 @@ private val DarkColorScheme = darkColorScheme(
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
-    secondary = PurpleGrey40,
+    secondary = ,
     tertiary =
 )
 
