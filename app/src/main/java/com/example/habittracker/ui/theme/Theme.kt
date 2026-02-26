ccead199defe99cc97d57f562bf5ee22a2e95fb2
WellB2 <well.santanasft@gmail.com>
2026-02-26 19:16:17 -0300

    rollback Theme.kt

--- app/src/main/java/com/example/habittracker/ui/theme/Theme.kt
+++ app/src/main/java/com/example/habittracker/ui/theme/Theme.kt
@@ -41,14 +41,7 @@ fun HabitTrackerTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
+
 
 
 }
