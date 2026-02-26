bc3e9490c66995f0a4377ff285025e953d1def5f
WellB2 <well.santanasft@gmail.com>
2026-02-26 19:25:31 -0300

    rollback Theme.kt

--- app/src/main/java/com/example/habittracker/ui/theme/Theme.kt
+++ app/src/main/java/com/example/habittracker/ui/theme/Theme.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+
 
 
 
