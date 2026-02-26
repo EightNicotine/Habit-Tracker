745575ae35094d3d1eb42ca98591a35b3a0eb741
WellB2 <well.santanasft@gmail.com>
2026-02-26 19:13:01 -0300

    rollback Theme.kt

--- app/src/main/java/com/example/habittracker/ui/theme/Theme.kt
+++ app/src/main/java/com/example/habittracker/ui/theme/Theme.kt
@@ -18,7 +18,7 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = ,
     secondary = ,
-    tertiary =""
+    tertiary =S
 )
 
 private val LightColorScheme = lightColorScheme(
