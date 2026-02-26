0d07254346b42e9ef154aa19c3b194b3a9658b37
WellB2 <well.santanasft@gmail.com>
2026-02-26 19:29:21 -0300

    rollback Theme.kt

--- app/src/main/java/com/example/habittracker/ui/theme/Theme.kt
+++ app/src/main/java/com/example/habittracker/ui/theme/Theme.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-
+import androidx.compose.material3.darkColorScheme
 
 
 
